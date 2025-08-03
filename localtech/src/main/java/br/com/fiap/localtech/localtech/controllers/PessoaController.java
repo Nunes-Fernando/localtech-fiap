42bd957c78ec35fa54b2b04b18c5fbdc9ae8ede4
Fernando <fn4586@gmail.com>
2025-08-03 11:24:48 -0300

    finalizando o dev das rotas de pessoas

--- localtech/src/main/java/br/com/fiap/localtech/localtech/controllers/PessoaController.java
+++ localtech/src/main/java/br/com/fiap/localtech/localtech/controllers/PessoaController.java
@@ -10,57 +10,51 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.localtech.localtech.entites.Pessoa;
-import br.com.fiap.localtech.localtech.entites.Veiculos;
 import br.com.fiap.localtech.localtech.service.PessoaService;
-import br.com.fiap.localtech.localtech.service.VeiculoService;
 
 @RestController
-@RequestMapping("/veiculos")
+@RequestMapping("/pessoas")  
 public class PessoaController {
 
-	private static final Logger log = LoggerFactory.getLogger(PessoaController.class);
+    private static final Logger log = LoggerFactory.getLogger(PessoaController.class);
 
-	private final PessoaService service;
+    private final PessoaService service;
 
-	public PessoaController(PessoaService service) {
-		this.service = service;
-	}
+    public PessoaController(PessoaService service) {
+        this.service = service;
+    }
 
-	@GetMapping
-	public ResponseEntity<List<Pessoa>> findAllVeiculos(@RequestParam("page") int page,
-			@RequestParam("size") int size) {
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> findAllPessoas(@RequestParam("page") int page,
+                                                      @RequestParam("size") int size) {
+        var pessoas = this.service.findAllVeiculos(page, size);  
+        return ResponseEntity.ok(pessoas);
+    }
 
-		var pessoa = this.service.findAllVeiculos(page, size);
-		return ResponseEntity.ok(pessoa);
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> findPessoa(@PathVariable("id") Long id) {
+        var pessoa = this.service.findById(id);
+        return ResponseEntity.ok(pessoa);
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Optional<Pessoa>> findPessoa(@PathVariable("id") Long id) {
-		var pessoa = this.service.findById(id);
-		return ResponseEntity.ok(pessoa);
-	}
+    @PostMapping
+    public ResponseEntity<Void> savePessoa(@RequestBody Pessoa pessoa) {
+        log.info("POST -> /pessoas");
+        service.saveVeiculo(pessoa);  
+        return ResponseEntity.ok().build();
+    }
 
-	@PostMapping
-	public ResponseEntity<Void> savePessoa(@RequestBody Pessoa pessoa) {
-		log.info("POST -> /veiculos");
-		service.saveVeiculo(pessoa);
-		return ResponseEntity.ok().build();
-	}
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updatePessoa(@PathVariable("id") Long id, @RequestBody Pessoa pessoa) {
+        log.info("PUT -> /pessoas/{}", id);
+        this.service.updateVeiculo(pessoa, id);  
+        return ResponseEntity.ok().build();
+    }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Void> updatePessoa(@PathVariable("id") Long id, @RequestBody Pessoa pessoa) {
-
-		log.info("PUT -> /veiculos/{}", id);
-		this.service.updateVeiculo(pessoa, id);
-		return ResponseEntity.ok().build();
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deletePessoa(@PathVariable("id") Long id) {
-	    
-	    log.info("DELETE -> /veiculos/{}", id);
-	    this.service.delete(id);
-	    return ResponseEntity.ok().build();
-	    
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePessoa(@PathVariable("id") Long id) {
+        log.info("DELETE -> /pessoas/{}", id);
+        this.service.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
